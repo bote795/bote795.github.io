0a3cccde0ae40c1ebb7aa614a17ad9f9bb4aebe1
bote795 <nbotell2@gmail.com>
2015-08-06 14:14:49 -0500

    major updates
    Updated information in website and added new projects...

--- js/mainInjection.js
+++ js/mainInjection.js
@@ -1,9 +1,11 @@
 //stores the data that will be used to inject into the fields
 function PrePopulatedData()
 {
-    var imagePath = ["../../images/Fixed.png","images/128.png", "images/TXDOT-Logo.jpg"];
-    var titles = ["iNationGaming", "Anime Tracker Storage", "Texas Department of Transportation"];
-    var bodys =["Mission – To become a well established brand/company with a huge fan base where we can host events for them. Not only can the fans interact with one another at events but they can also enjoy video content which includes Youtube and Twitch.","<b>Tired of forgeting what episode you are in?</b><br>AnimeTrackerS.(ATS) is a chrome extension that will let you have a list of the animes you are currently watching. But as you watch new episodes or the next episode, it will be updating what episode you are in and let you know when a new one is up. <br><b>It is now on the chrome app store! Still ongoing further updates!</b>","Worked as an Intern for two summers and during my high school senior year. I worked in various departments and learned Visual Basic to help them develop macros to speed up efficiency. While working in teams to draw up road way plans with the use of microstation."];
+    var imagePath = ["../../images/Fixed.png","images/128.png", "images/Verizon_Logo_2015.jpg"];
+    var titles = ["iNationGaming", "Anime Tracker Storage", "Verizon"];
+    var bodys =["Mission – To become a well established brand/company with a huge fan base where we can host events for them. Not only can the fans interact with one another at events but they can also enjoy video content which includes Youtube and Twitch.",
+                "<b>Tired of forgeting what episode you are in?</b><br>AnimeTrackerS.(ATS) is a chrome extension that will let you have a list of the animes you are currently watching. But as you watch new episodes or the next episode, it will be updating what episode you are in and let you know when a new one is up. <br><b>It is now on the chrome app store! Still ongoing further updates!</b>",
+                "Worked as an Intern for two summers. I worked in a team that works in an internal tool that supports over 190k users and delivers the entire end-to-end application life cycle functions from Requirements-to-Production. First summer developed server scripts to return information to database (SQL) and worked on GUI using PHP, HTML, JavaScript and CSS. Second summer worked on enhancements to the system GUI and UX."];
     var pathToWebpages = ['./Pages/projects/iNationGaming.html','Pages/projects/ATS.html','Pages/main/work.html'];
     var results=[];
     for (var i = 0; i < imagePath.length; i++) {
