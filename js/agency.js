809e2846180318720cdba1dcfe8d9656559c7b9c
bote795 <nbotell2@gmail.com>
2016-05-13 15:07:55 -0500

    add GA

--- js/agency.js
+++ js/agency.js
@@ -8,10 +8,15 @@
 $(function() {
     $('a.page-scroll').bind('click', function(event) {
         var $anchor = $(this);
+        ga('send', 'event', "clicked heading", this.text);
         $('html, body').stop().animate({
             scrollTop: $($anchor.attr('href')).offset().top
         }, 1500, 'easeInOutExpo');
         event.preventDefault();
+    });
+    $('a.portfolio-link').bind('click',function(event){
+         var clickedItem =this.parentElement.lastElementChild.firstElementChild.textContent;
+         ga('send', 'event', "viewed element", clickedItem);
     });
 });
 
